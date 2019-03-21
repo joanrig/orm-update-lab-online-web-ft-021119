320aa30fcbe80830acd5b0dbb310d1a4971ca3b6
Joan Lyness <joanrigdon@gmail.com>
2019-03-21 06:15:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,9 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+      
   end
 
 
